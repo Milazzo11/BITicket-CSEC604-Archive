abaf725f523db0f2da7ec3106f6f092cc2367c7f
Ashim Mahara <mr.maharaofficial@outlook.com>
2024-11-30 13:32:16 -0500

    renamed to correct api calls

--- src/server.py
+++ src/server.py
@@ -2,7 +2,6 @@
 API endpoints
 """
 
-
 from app.API import API
 from app.API.models import *
 from fastapi import FastAPI
@@ -18,12 +17,12 @@ async def search_events(data: Auth[SearchRequest]) -> Auth[SearchResponse]:
 
 @app.post("/create", description="Create a new event on the server")
 async def create_event(data: Auth[CreateRequest]) -> Auth[CreateResponse]:
-    return API.search_events(data)
+    return API.create_event(data)
 
 
 @app.post("/register", description="Register a user for an event and return his ticket")
 async def register_user(data: Auth[RegisterRequest]) -> Auth[RegisterResponse]:
-    return API.search_events(data)
+    return API.register_user(data)
 
 
 @app.post("/cancel", description="Cancel event registration (return ticket)")
@@ -31,7 +30,10 @@ async def cancel_ticket(data: Auth[CancelRequest]) -> Auth[CancelResponse]:
     return API.cancel_ticket(data)
 
 
-@app.post("/transfer", description="Return a ticket back to the server or transfer it to another user")
+@app.post(
+    "/transfer",
+    description="Return a ticket back to the server or transfer it to another user",
+)
 async def transfer_ticket(data: Auth[TransferRequest]) -> Auth[TransferResponse]:
     return API.transfer_ticket(data)
 
@@ -41,8 +43,9 @@ async def redeem_ticket(data: Auth[RedeemRequest]) -> Auth[RedeemResponse]:
     return API.redeem_ticket(data)
 
 
-@app.get("/verify", description="Verify (as an event owner) that a user has redeemed his ticket")
+@app.get(
+    "/verify",
+    description="Verify (as an event owner) that a user has redeemed his ticket",
+)
 async def verify_redemption(data: Auth[VerifyRequest]) -> Auth[VerifyResponse]:
     return API.verify_redemption(data)
-
-
